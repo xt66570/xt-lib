cdfb353eacb83b5b54cc747f479f5a3e8323557e
xt <1799250230@qq.com>
2021-06-16 15:18:18 +0800

    feat version1.0

---
+++ lib/lib-api/src/main/java/com/xt/lib/api/UserService.java
@@ -0,0 +1,14 @@
+package com.xt.lib.api;
+
+
+import com.xt.lib.api.model.UserModel;
+
+/**
+ * @author <a href="mailto:chenxilzx1@gmail.com">theonefx</a>
+ */
+public interface UserService {
+
+    String getUserName(Long id);
+
+    UserModel addUser(UserModel user);
+}
